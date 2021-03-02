3e5d8081cd405b259a15aab338542ccbf1d59898
Petr Frolkin <petrooha@gmail.com>
2021-03-02 09:11:07 -0500

    Add files via upload

---
+++ Gradient Descent (How weights get updated in NN) 3 EXAMPLES (1 node, 1 hidden layer).py
@@ -0,0 +1,124 @@
+import numpy as np
+
+# Defining the sigmoid function for activations
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+# Derivative of the sigmoid function
+def sigmoid_prime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+# Input data
+x = np.array([0.1, 0.3])
+# Target
+y = 0.2
+# Input to output weights
+weights = np.array([-0.8, 0.5])
+
+# The learning rate, eta in the weight step equation
+learnrate = 0.5
+
+# the linear combination performed by the node (h in f(h) and f'(h))
+h = x[0]*weights[0] + x[1]*weights[1]
+# or h = np.dot(x, weights)
+
+# The neural network output (y-hat)
+nn_output = sigmoid(h)
+
+# output error (y - y-hat)
+error = y - nn_output
+
+# output gradient (f'(h))
+output_grad = sigmoid_prime(h)
+
+# error term (lowercase delta)
+error_term = error * output_grad
+
+# Gradient descent step 
+del_w = [ learnrate * error_term * x[0],
+          learnrate * error_term * x[1]]
+# or del_w = learnrate * error_term * x
+
+
+
+######################################
+###########################################
+
+
+# MORE COMPLEX IMPLEMENTATION
+# dont run the code  /  features file AND hot encoding missing
+"""
+# Use to same seed to make debugging easier
+np.random.seed(42)
+
+n_records, n_features = features.shape
+last_loss = None
+
+# Initialize weights
+weights = np.random.normal(scale=1 / n_features**.5, size=n_features)
+
+# Neural Network hyperparameters
+epochs = 1000
+learnrate = 0.5
+
+for e in range(epochs):
+    del_w = np.zeros(weights.shape)
+    for x, y in zip(features.values, targets):
+        # Loop through all records, x is the input, y is the target
+
+        # Note: We haven't included the h variable from the previous
+        #       lesson. You can add it if you want, or you can calculate
+        #       the h together with the output
+
+        # TODO: Calculate the output
+        h = np.dot(x, weights)
+        output = sigmoid(h)
+
+        # TODO: Calculate the error
+        error = y - output
+
+        # TODO: Calculate the error term
+        error_term = error * output * (1 - output)
+
+        # TODO: Calculate the change in weights for this sample
+        #       and add it to the total weight change
+        del_w += error_term * x
+
+    # TODO: Update weights using the learning rate and the average change in weights
+    weights += learnrate * del_w / n_records
+
+"""
+
+##################################################
+
+###########################################
+
+#  HIDDEN LAYER
+
+
+# Network size
+N_input = 4
+N_hidden = 3
+N_output = 2
+
+np.random.seed(42)
+# Make some fake data
+X = np.random.randn(4)
+
+weights_input_to_hidden = np.random.normal(0, scale=0.1, size=(N_input, N_hidden))
+weights_hidden_to_output = np.random.normal(0, scale=0.1, size=(N_hidden, N_output))
+
+
+# TODO: Make a forward pass through the network
+
+hidden_layer_in = np.dot(X, weights_input_to_hidden)
+hidden_layer_out = sigmoid(hidden_layer_in)
+
+print('Hidden-layer Output:')
+print(hidden_layer_out)
+
+output_layer_in = np.dot(hidden_layer_out, weights_hidden_to_output)
+output_layer_out = sigmoid(output_layer_in)
+
+print('Output-layer Output:')
+print(output_layer_out)
